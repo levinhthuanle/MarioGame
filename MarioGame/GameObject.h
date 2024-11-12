72bec782d64ce1b3185b98452bf42536bdc9eba4
Thuanle27 <thuanle.qt@gmail.com>
2024-11-13 00:35:49 +0700

    feat: Add GameObject class

---
+++ MarioGame/GameObject.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <SFML\Graphics.hpp>
+#include <iostream>
+class GameObject
+{
+public:
+	sf::Texture m_texture;
+	sf::Sprite m_sprite;
+public:
+	GameObject() {};
+
+	GameObject(std::string path) {
+		if (!m_texture.loadFromFile(path)) {
+			std::cerr << "Can not load Resources \n";
+			return;
+		}
+
+		m_sprite.setTexture(m_texture);
+	}
+
+	void load(std::string path) {
+		if (!m_texture.loadFromFile(path)) {
+			std::cerr << "Can not load Resources \n";
+			return;
+		}
+		m_sprite.setTexture(m_texture);
+	}
+
+	void draw(sf::RenderWindow& window, float x, float y) {
+		m_sprite.setPosition(x, y);
+		window.draw(m_sprite);
+	}
+
+	sf::FloatRect getBounds() {
+		return m_sprite.getLocalBounds(); // ?? Does it correct
+	}
+};
+
